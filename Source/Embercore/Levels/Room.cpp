e96bcccab53d9574455a76101ddf294c65e55ac4
Nickolas Komarnitsky <komarnickolas@gmail.com>
2022-05-12 00:03:01 -0600

    Add Initial Room Setup

---
+++ Source/Embercore/Levels/Room.cpp
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Room.h"
+
+
+// Sets default values
+ARoom::ARoom(const class FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer) {
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ARoom::BeginPlay() {
+	Super::BeginPlay();
+}
+
+// Called every frame
+void ARoom::Tick(float DeltaTime) {
+	Super::Tick(DeltaTime);
+}
+
+void ARoom::Generate() {
+	FString Info = FString::Printf(TEXT("%1i,%1i"), RoomWidth, RoomHeight);
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *Info);
+	for (float x = 0.0; x < RoomWidth * FloorWidth; x += FloorWidth) {
+		for (float y = 0.0; y < RoomHeight * FloorHeight; y += FloorHeight) {
+			FString Name = FString::Printf(TEXT("%3.3f,%3.3f"), x, y);
+			UE_LOG(LogTemp, Warning, TEXT("%s"), *Name);
+			Floor.Add(NewObject<UStaticMeshComponent>(RootComponent, UStaticMeshComponent::StaticClass(),
+			                                          FName(*Name)));
+			Floor.Last()->SetStaticMesh(FloorMesh);
+			Floor.Last()->SetMaterial(0, FloorMaterial);
+			Floor.Last()->SetRelativeLocation(FVector(x, y, 0.0f));
+			Floor.Last()->SetupAttachment(RootComponent);
+			Floor.Last()->RegisterComponent();
+		}
+	}
+}
+
+
+float ARoom::GetRandomFloat(const float Max, const float Min = 0.0) {
+	return Min + static_cast<float>(rand()) / (RAND_MAX / (Max - Min));
+}
---
+++ Source/Embercore/Levels/Room.h
@@ -0,0 +1,56 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Room.generated.h"
+
+UCLASS(Blueprintable)
+class EMBERCORE_API ARoom : public AActor {
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ARoom(const class FObjectInitializer& ObjectInitializer);
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	int32 RoomWidth;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	int32 RoomHeight;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Floor")
+	float FloorWidth;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Floor")
+	float FloorHeight;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Floor")
+	UStaticMesh* FloorMesh;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Floor")
+	UMaterial* FloorMaterial;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Wall")
+	float WallWidth;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Wall")
+	float WallHeight;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Wall")
+	UStaticMesh* WallMesh;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Wall")
+	UMaterial* WallMaterial;
+	
+	UPROPERTY(EditAnywhere)
+	TArray<UStaticMeshComponent*> Floor;
+
+	UPROPERTY(EditAnywhere)
+	TArray<UStaticMeshComponent*> Wall;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable)
+	void Generate();
+	float GetRandomFloat(float Max, float Min);
+};
