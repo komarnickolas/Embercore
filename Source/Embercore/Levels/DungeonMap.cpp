1b7aef908c64b001108a948037d6432abe56d259
Nickolas Komarnitsky <komarnickolas@gmail.com>
2022-05-15 14:09:28 -0600

    Starting Code For Dungeon Generation

---
+++ Source/Embercore/Levels/DungeonMap.cpp
@@ -0,0 +1,67 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DungeonMap.h"
+
+// FVector4(X=X,Y=Y,Z=Height,W=Width)
+
+UDungeonMap::UDungeonMap() {
+}
+
+void UDungeonMap::DrawDebug() {
+	DrawDebugNode(0);
+}
+
+void UDungeonMap::DrawDebugNode(int32 NodeIndex) {
+	const FDungeonTree Node = Nodes[NodeIndex];
+	float X = Node.Container.X;
+	float Y = Node.Container.Y;
+	float XMax = Node.Container.X + Node.Container.W;
+	float YMax = Node.Container.Y + Node.Container.Z;
+	DrawDebugLine(GetWorld(), FVector(X, Y, 0), FVector(XMax, Y, 0), FColor::Green);
+	DrawDebugLine(GetWorld(), FVector(XMax, Y, 0), FVector(XMax, YMax, 0), FColor::Green);
+	DrawDebugLine(GetWorld(), FVector(X, YMax, 0), FVector(XMax, YMax, 0), FColor::Green);
+	DrawDebugLine(GetWorld(), FVector(X, Y, 0), FVector(X, YMax, 0), FColor::Green);
+	if (Node.Left != 0) { DrawDebugNode(Node.Left); }
+	if (Node.Right != 0) { DrawDebugNode(Node.Right); }
+}
+
+void UDungeonMap::GenerateMap(FRandomStream InStream) {
+	this->Stream = InStream;
+	SplitDungeon(Depth, FVector4(0, 0, Size, Size));
+	UE_LOG(LogTemp, Warning, TEXT("Dungeon Generated"));
+}
+
+TArray<FVector4> UDungeonMap::SplitDungeonContainer(FVector4 Container) {
+	TArray<FVector4> Array;
+	FVector4 C1, C2;
+	const FVector2D SplitPosition = RandomPosition(Container);
+	if (SplitPosition.X == 0) {
+		C1 = FVector4(Container.X, Container.Y, Stream.FRandRange(Container.Z * 0.3f, Container.Z * 0.5f), Container.W);
+		C2 = FVector4(Container.X, Container.Y + C1.Z, Container.Z - C1.Z, Container.W);
+	}
+	else {
+		C1 = FVector4(Container.X, Container.Y, Container.Z, Stream.FRandRange(Container.W * 0.3f, Container.W * 0.5f));
+		C2 = FVector4(Container.X + C1.W, Container.Y, Container.Z, Container.W - C1.W);
+	}
+	Array.Add(C1);
+	Array.Add(C2);
+	return Array;
+}
+
+int32 UDungeonMap::SplitDungeon(int32 iteration, FVector4 Container) {
+	const int32 Index = Nodes.Add(FDungeonTree(Container));
+	if (iteration == 0) { return Index; }
+
+	TArray<FVector4> SplitContainers = SplitDungeonContainer(Container);
+	Nodes[Index].Left = SplitDungeon(iteration - 1, SplitContainers[0]);
+	Nodes[Index].Right = SplitDungeon(iteration - 1, SplitContainers[1]);
+	return Index;
+}
+
+FVector2D UDungeonMap::RandomPosition(FVector4 Container) {
+	if (Stream.RandRange(0, 1) == 0) {
+		return FVector2D(Stream.RandRange(0, Container.Z), 0);
+	}
+	return FVector2D(0, Stream.RandRange(0, Container.W));
+}
---
+++ Source/Embercore/Levels/DungeonMap.h
@@ -0,0 +1,55 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "DungeonMap.generated.h"
+USTRUCT(BlueprintType)
+struct EMBERCORE_API FDungeonTree {
+	GENERATED_USTRUCT_BODY()
+public:
+	UPROPERTY(BlueprintReadOnly)
+	FVector4 Container;
+	UPROPERTY(BlueprintReadOnly)
+	int32 Index;
+	UPROPERTY(BlueprintReadOnly)
+	int32 Left;
+	UPROPERTY(BlueprintReadOnly)
+	int32 Right;
+
+	FDungeonTree(): Index(0), Left(0), Right(0) {
+	}
+
+	FDungeonTree(FVector4 C): Index(0), Left(0), Right(0) {
+		this->Container = C;
+	}
+};
+
+/**
+ * 
+ */
+UCLASS(BlueprintType, Blueprintable)
+class EMBERCORE_API UDungeonMap : public UObject {
+	GENERATED_BODY()
+public:
+	UDungeonMap();
+	UFUNCTION(BlueprintCallable)
+	void DrawDebug();
+	void DrawDebugNode(int32 NodeIndex);
+	UFUNCTION(BlueprintCallable)
+	void GenerateMap(FRandomStream Stream);
+	TArray<FVector4> SplitDungeonContainer(FVector4 Container);
+	int32 SplitDungeon(int32 iteration, FVector4 Container);
+	FVector2D RandomPosition(FVector4 Container);
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	int32 Size;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	int32 Depth;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	int32 MinRoomDelta;
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TArray<FDungeonTree> Nodes;
+	UPROPERTY(VisibleAnywhere)
+	FRandomStream Stream;
+};
